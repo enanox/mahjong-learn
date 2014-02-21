9e1958a4fb72e0f32f64cdf2b60d8a66a5889822
Mathias Rodriguez <mathias.rodriguez@gmail.com>
2014-02-21 16:09:41 -0200

    Added yeoman support for project

---
+++ app/scripts/controllers/table.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('mahjongLearnAppApp')
+  .controller('TableCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/welcomeroute.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('mahjongLearnAppApp')
+  .controller('WelcomerouteCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
