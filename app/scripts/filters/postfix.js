d18a1453e6f0105b9ba28bc17a8bef7a70c6d244
Mathias Rodriguez <mathias.rodriguez@gmail.com>
2014-03-24 09:23:27 -0300

    Fixed test errors in postfix filter

--- app/scripts/filters/postfix.js
+++ app/scripts/filters/postfix.js
@@ -1,10 +1,15 @@
 'use strict';
 
-angular.module('mahjongLearnAppApp')
-  .filter('postfix', function (L10n) {
-    return function (input) {
-    	var postfixesByLanguage = L10n.getTexts() ? L10n.getTexts().postfixNumbers[L10n.getLanguage()] : 'st';
-    	
-      return input <= 4 ? input + '' + postfixesByLanguage[input - 1] : input + '' + postfixesByLanguage[3];
-    };
-  });
+angular.module('mahjongLearnAppApp').filter(
+    'postfix',
+    function(L10n) {
+	    return function(input) {
+		    var postfixesByLanguage = L10n.getTexts()
+		        && L10n.getTexts().hasOwnProperty('postfixNumbers') ? L10n
+		        .getTexts().postfixNumbers[L10n.getLanguage()] : [ 'st', 'nd',
+		        'rd', 'th' ];
+
+		    return input <= 4 ? input + '' + postfixesByLanguage[input - 1] : input
+		        + '' + postfixesByLanguage[3];
+	    };
+    });
