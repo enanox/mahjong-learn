4007a50ff22728a2c6d06a90aeee567144d1a264
Mathias Rodriguez <mathias.rodriguez@gmail.com>
2014-03-24 09:16:57 -0300

    Starting contact form with validations

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -1,6 +1,18 @@
 'use strict';
 
 angular.module('mahjongLearnAppApp')
-  .controller('ContactCtrl', function ($scope) {
+  .controller('ContactCtrl', function ($scope, L10n) {
+  	$scope.language = $scope.getLanguage();
+    $scope.setLanguage($scope.language);
     
+    $scope.localize = function(lang) {
+    	$scope.setLanguage(lang);
+    };
+	   
+    $scope.$on('languageChange', function(a) {
+    	$scope.language = $scope.getLanguage();
+    });
+    
+    // Got the texts from NavbarCtrl
+    $scope.texts = $scope.getTexts() ? $scope.getTexts().Contact : null;
   });
